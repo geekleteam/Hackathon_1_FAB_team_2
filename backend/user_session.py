563ab426981d74e39580049f0dbd275c176fc198
Yash Thakre <86224563+yash9904@users.noreply.github.com>
2024-07-16 11:05:04 +0530

    add db support

--- backend/user_session.py
+++ backend/user_session.py
@@ -1,4 +1,7 @@
+import json
 from typing import Dict, List, Optional
+
+from db_utils import fetch_user_chat_from_db, push_user_chat_to_db
 
 
 class ChatSession:
@@ -16,8 +19,15 @@ class ChatSession:
         self.chats: List[Dict[str, str]] = []
         self.history: List[Dict[str, str]] = []
 
-    def add_chat(self, user_input, model_output):
-        self.chats.append({"user": user_input, "model": model_output})
+    def add_chat(self, user_input, model_output, conn):
+        chat_dict = {"user": json.dumps(user_input), "model": json.dumps(model_output)}
+        self.chats.append(chat_dict)
+        push_user_chat_to_db(self.user_id, self.request_id, chat_dict, conn)
+
+    def populate_chat_from_db(self, user_id, request_id, conn):
+        self.chats = fetch_user_chat_from_db(user_id, request_id, conn)
+        self.user_id = user_id
+        self.request_id = request_id
 
     def flush(self):
         self.history = self.chats
@@ -29,17 +39,19 @@ class ChatSession:
 
     def str_chat(self) -> str:
         return "\n".join(
-            [f"User: {chat['user']}\nModel:{chat['model']}" for chat in self.chats]
+            [f"User: {chat['user']}\nBot:{chat['model']}" for chat in self.chats]
         )
 
 
 class ChatSessionManager:
-    def __init__(self):
+    def __init__(self, conn):
         self.sessions: Dict[str, ChatSession] = {}
+        self.conn = conn
 
-    def get_session(self, user_id: str) -> ChatSession:
+    def get_session(self, user_id: str, request_id: str) -> ChatSession:
         if user_id not in self.sessions:
             self.sessions[user_id] = ChatSession()
+            self.sessions[user_id].populate_chat_from_db(user_id, request_id, self.conn)
         return self.sessions[user_id]
 
     def remove_session(self, user_id: str):
