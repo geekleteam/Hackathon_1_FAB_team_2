30863b6de09a110a480807d7dbec9003e1c33ad8
Yash Thakre <86224563+yash9904@users.noreply.github.com>
2024-07-16 14:20:12 +0530

    update question prompt

--- backend/main.py
+++ backend/main.py
@@ -93,14 +93,16 @@ def chat_llm_no_stream(request: RequestModel, chat_session: ChatSession) -> dict
         text_input = initial_context + text_input
     else:
         text_input = f"""
-            Given the following conversation of chatbot and user:
+            Given the following conversation history of you and user:
+            <start>
             {chat_session.str_chat()}
+            <end>
             and the latest response from the user: {request.user_input}
             Try to suggest the options for the user to choose from along with the question. 
             Please vary the type of questions (yes/no, multiple choice, open-ended, etc.) to get the required information.
             Only ask the question and no extra text. And if the user is not sure or confused, suggest options.
             DO NOT REPEAT the questions already answered by the user in the conversation above.
-            Ask one subsequent question in short with suggestions if necessary. Only ask ONE question and no extra text.
+            Ask one subsequent question with suggestions if necessary. Only ask ONE question.
         """
 
     response = chat_model.invoke(text_input)
